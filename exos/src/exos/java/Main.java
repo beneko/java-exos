f3a522f759386b74f6e54b6cb0ca69773df8e424
beneko <behzadnekouei@gmail.com>
2021-07-05 14:21:54 +0200

    exos 1 done

---
+++ exos/src/exos/java/Main.java
@@ -0,0 +1,16 @@
+package exos.java;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Please input a number:");
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        System.out.println("Please input another number:");
+        int num2 = sc.nextInt();
+        System.out.println(num1 + num2);
+
+    }
+}
