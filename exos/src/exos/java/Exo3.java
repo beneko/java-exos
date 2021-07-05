0cf061a2cd0c4ac33c3fc0d3e4bf6b65dc61775c
beneko <behzadnekouei@gmail.com>
2021-07-05 14:59:37 +0200

    exos 3 done

---
+++ exos/src/exos/java/Exo3.java
@@ -0,0 +1,16 @@
+package exos.java;
+
+import java.util.Scanner;
+
+public class Exo3 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a temperature in Fahrenheit:");
+        float f = scanner.nextFloat();
+        float c = ((5f / 9f) * (f - 32));
+        System.out.println(c);
+
+    }
+}
--- exos/src/exos/java/Main.java
+++ exos/src/exos/java/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Please input a number:");
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
