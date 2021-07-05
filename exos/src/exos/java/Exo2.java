4637f9507dd229dfa2bd8250e9c750902455a987
beneko <behzadnekouei@gmail.com>
2021-07-05 14:38:33 +0200

    exos 2 done

--- exos/src/exos/java/Exo2.java
+++ exos/src/exos/java/Exo2.java
@@ -2,15 +2,15 @@ package exos.java;
 
 import java.util.Scanner;
 
-public class Main {
+public class Exo2 {
 
     public static void main(String[] args) {
         System.out.println("Please input a number:");
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
+        float num1 = sc.nextInt();
         System.out.println("Please input another number:");
-        int num2 = sc.nextInt();
-        System.out.println(num1 + num2);
+        float num2 = sc.nextInt();
+        System.out.println(num1 / num2);
 
     }
 }
