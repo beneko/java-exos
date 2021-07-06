75549f16194f14cd7a07dd2330b76013d919a61e
beneko <behzadnekouei@gmail.com>
2021-07-06 15:23:58 +0200

    exos 15 done

---
+++ exos/src/exos/java/Exo15.java
@@ -0,0 +1,71 @@
+package exos.java;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Scanner;
+
+public class Exo15 {
+    /*
+    Un palindrome est un mot que l'on peut lire dans les deux sens.
+    La distinction entre majuscules/minuscules n'a aucune importance pour la lecture d'un palindrome.
+     Si on ne tient pas compte des espaces, des virgules, des tirets et des apostrophes (i.e. les caractères ' ', ',', '-' et '\''),
+      une phrase complète peut aussi être considérée comme un palindrome.
+      Exemples de palindromes: Otto Elu par cette crapule Esope reste ici et se repose Tu l'as trop ecrase,
+       Cesar, ce Port-Salut A man, a plan, a canal, Panama Exemples de non-palindromes:
+        Cours de Java Le pont de la rivière Kwai Ecrivez un programme Palindrome.
+        java qui lit une chaine de caractères du clavier et qui teste s'il s'agit d'un palindrome.
+         Exemple d'exécution: Entrez un mot ou une phrase : Otto C'est un palindrome ! Pour ce programme,
+         il convient d'utiliser plusieurs méthodes prédéfinies de la classe String, comme par exemple charAt(),
+          toLowerCase() et length(). chaine.toLowerCase() permet de convertir tous les caratères de chaine en minuscule.
+     */
+    public static void main(String[] args) {
+
+        String words;
+        String inverse;
+        int length;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Veuillez entrer un mot ou une phrase: ");
+        words = scanner.next();
+        words = words.toLowerCase();
+        length = words.length();
+        boolean palindrome = true;
+
+        for (int i = 0; i < length; i++) {
+            char chr = words.charAt(i);
+            switch (chr) {
+                case ' ':
+                   words = words.replace(" ", "");
+                case ',':
+                    words = words.replace(",", "");
+                case '-':
+                    words = words.replace("-", "");
+                case '\'':
+                    words = words.replace("\'", "");
+            }
+        }
+        length = words.length(); // update length
+        StringBuilder str = new StringBuilder(words);
+        inverse = str.reverse().toString();
+
+        for (int i = 0; i < length; i++) {
+
+            if (words.charAt(i) != inverse.charAt(i)) {
+
+                palindrome = false;
+                break;
+
+            }
+        }
+        if (palindrome){
+
+            System.out.println("C'est un palindrome!");
+
+        }else{
+
+            System.out.println("Ce n'est pas un palindrome!");
+
+        }
+    }
+
+}
