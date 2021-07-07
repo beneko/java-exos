1715413c7f0014cbaf5ad14dcc3b0a6e8521de60
beneko <behzadnekouei@gmail.com>
2021-07-07 16:56:13 +0200

    change directories

---
+++ exos/src/org/afpa/Main.java
@@ -0,0 +1,11 @@
+package org.afpa;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ exos/src/org/afpa/exos/Exo6.java
@@ -0,0 +1,23 @@
+package org.afpa.exos;
+
+import java.util.Scanner;
+
+public class Exo6 {
+    /*
+     ASCII
+    Écrire un programme qui saisit un code Unicode en décimal et affiche le caractère correspondant.
+    Exemple: la saisie de l’entier 65 donne le caractère « A ».
+     */
+    public static void main(String[] args) {
+
+        int asci;
+        char chr;
+
+        System.out.println("Please enter a number:");
+        Scanner scanner = new Scanner(System.in);
+        asci = scanner.nextInt();
+        chr = (char) asci;
+
+        System.out.println("it represent \'"+ chr + "\' character.");
+    }
+}
