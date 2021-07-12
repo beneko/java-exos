2ca2216df81dc469355c3f714170bb83ddecc292
beneko <behzadnekouei@gmail.com>
2021-07-12 10:13:20 +0200

    exo 25 updated

--- exos/src/org/afpa/model/Fonctions.java
+++ exos/src/org/afpa/model/Fonctions.java
@@ -111,7 +111,7 @@ public class Fonctions {
      */
     public static void tableMultiplication(int num){
 
-        for (int i=1 ; i<Math.abs(num) ; i++){
+        for (int i=1 ; i<=10 ; i++){
 
             System.out.println(i + " x " + num + " = "+ (i*num));
 
